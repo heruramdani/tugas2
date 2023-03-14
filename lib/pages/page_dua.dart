62f6cce01f94fcd992a2a967b1afa7c1e49cf8e5
heruramdani <82434298+heruramdani@users.noreply.github.com>
2023-03-14 10:59:50 +0700

    Update page_dua.dart

--- lib/pages/page_dua.dart
+++ lib/pages/page_dua.dart
@@ -10,62 +10,20 @@ class PageDua extends StatelessWidget {
         leading: Container(),
       ),
       body: Center(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  'Halo Deck',
-                  style:
-                      TextStyle(fontSize: 24, fontFamily: "Serif", height: 2.0),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      margin: EdgeInsets.only(bottom: 40, left: 50),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      margin: EdgeInsets.only(bottom: 40, right: 50),
-                    ),
-                  ],
-                )
-              ],
+            Container(
+              color: Colors.green,
             ),
             Container(
-              height: 300,
-              width: 300,
-              margin: EdgeInsets.only(bottom: 40),
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(16)),
+              color: Colors.red,
+              height: 400.0,
+              width: 300.0,
             ),
             Container(
-              child: Text(
-                'Heru Ramdani',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Text(
-                '1197070049',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-              ),
+              color: Colors.deepPurple,
+              height: 200.0,
+              width: 200.0,
             ),
           ],
         ),
